bc706f02bcce4334b2d8907193d1c42413ffe631
Danial Sakhpantayev <danial.sakhpantayev@gmail.com>
2024-11-20 20:40:58 +0500

    cors added

--- farmer_market_backend/settings.py
+++ farmer_market_backend/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     "users",
     "corsheaders",
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
