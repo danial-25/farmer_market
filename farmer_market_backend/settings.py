700497e8bf932c6d963a2778aaad42ceb1451f85
Danial Sakhpantayev <danial.sakhpantayev@gmail.com>
2024-11-20 21:03:15 +0500

    cors fix

--- farmer_market_backend/settings.py
+++ farmer_market_backend/settings.py
@@ -62,18 +62,23 @@ INSTALLED_APPS = [
     "users",
     "corsheaders",
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://farmer-market-33zm.onrender.com",
+    "http://localhost:3000",  # If your frontend is running on this port
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # Ensure it's here, before CSRF and Authentication
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
 ]
+
 
 ROOT_URLCONF = "farmer_market_backend.urls"
 
