e8fb3d743562bf469bd4977fbe1897b28607f4da
Danial Sakhpantayev <danial.sakhpantayev@gmail.com>
2024-11-28 00:13:12 +0500

    chat fix

--- chats/urls.py
+++ chats/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import MyInbox, GetMessages, SendMessages
 
 urlpatterns = [
-    path('my-messages/<int:user_id>/', MyInbox.as_view(), name='my-inbox'),
-    path('get-messages/<int:sender_id>/<int:receiver_id>/', GetMessages.as_view(), name='get-messages'),
-    path('send-messages/', SendMessages.as_view(), name='send-messages'),
+    path("my-messages/", MyInbox.as_view(), name="my-inbox"),
+    path(
+        "get-messages/<int:sender_id>/<int:receiver_id>/",
+        GetMessages.as_view(),
+        name="get-messages",
+    ),
+    path("send-messages/", SendMessages.as_view(), name="send-messages"),
 ]
